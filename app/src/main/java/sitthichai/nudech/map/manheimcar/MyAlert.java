79311be3c4e32e6243409a70a46200bf50f280cf
SitthichaiN <sitthichai.n@gmail.com>
2016-10-22 16:09:39 +0700

    End of first day.

---
+++ app/src/main/java/sitthichai/nudech/map/manheimcar/MyAlert.java
@@ -0,0 +1,41 @@
+package sitthichai.nudech.map.manheimcar;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by man on 22/10/2559.
+ */
+
+public class MyAlert {
+
+    // Explicit
+    private Context context;
+    private int anInt;
+    private String titleString, messageString;
+
+    public MyAlert(Context context, int anInt, String titleString, String messageString) {
+        this.context = context;
+        this.anInt = anInt;
+        this.titleString = titleString;
+        this.messageString = messageString;
+    }
+
+    public void myDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(anInt);
+        builder.setTitle(titleString);
+        builder.setMessage(messageString);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    } // myDialog
+}
+
+
--- app/src/main/java/sitthichai/nudech/map/manheimcar/SignUpActivity.java
+++ app/src/main/java/sitthichai/nudech/map/manheimcar/SignUpActivity.java
@@ -2,12 +2,57 @@ package sitthichai.nudech.map.manheimcar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import static sitthichai.nudech.map.manheimcar.R.id.button;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    // Explicit
+
+    private EditText nameEditText, userEditText, passwordEditText;
+    private ImageView imageView;
+    private Button button;
+    private String nameString, userString, passwordString, imageString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        // Bind widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button3);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Gey value from edit text
+                nameString = nameEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                // Check space
+                if (nameString.length() == 0 ||
+                        userString.length() == 0 ||
+                        passwordString.length() == 0 ) {
+                    // Have space
+                    MyAlert myAlert = new MyAlert(SignUpActivity.this,
+                            R.drawable.bird48, "มีช่องว่าง", "กรุณากรอกทุกช่อง เด้อ!");
+                    myAlert.myDialog();
+                }
+            }
+        });
+
+
+
+    }   // Method
+}   // Main
