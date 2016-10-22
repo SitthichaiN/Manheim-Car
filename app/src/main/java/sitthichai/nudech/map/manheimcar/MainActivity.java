358e3465ee7e58caf396a6cf31806abc42a9ebf2
SitthichaiN <sitthichai.n@gmail.com>
2016-10-22 13:55:34 +0700

    Create SignUp Activity - layout design.

--- app/src/main/java/sitthichai/nudech/map/manheimcar/MainActivity.java
+++ app/src/main/java/sitthichai/nudech/map/manheimcar/MainActivity.java
@@ -1,14 +1,36 @@
 package sitthichai.nudech.map.manheimcar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    /*
+        Explicit {Access Type Name}
+    */
+
+    private Button signInButton, signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Test push
-    }
+
+        // Bind Widget
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2);
+
+        // SignUp Controller
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent = move from page to other page.
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
+    }   // Main Method
 }   // Main class
