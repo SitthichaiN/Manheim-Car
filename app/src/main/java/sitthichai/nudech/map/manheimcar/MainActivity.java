f7103a5dc245471f7a4a35d1c1ed7f823c066ca6
SitthichaiN <sitthichai.n@gmail.com>
2016-10-24 10:33:43 +0700

    Read JSON Finish

--- app/src/main/java/sitthichai/nudech/map/manheimcar/MainActivity.java
+++ app/src/main/java/sitthichai/nudech/map/manheimcar/MainActivity.java
@@ -1,11 +1,18 @@
 package sitthichai.nudech.map.manheimcar;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -52,10 +59,57 @@ public class MainActivity extends AppCompatActivity {
                     MyAlert myAlert = new MyAlert(MainActivity.this, R.drawable.nobita48,
                             "Have space", "Please fill value");
                     myAlert.myDialog();
+                } else {
+
+                    // NoSpace
+                    MyConstants myConstants = new MyConstants();
+                    SynData synData = new SynData(MainActivity.this);
+                    synData.execute(myConstants.getUrlJSONString(),
+                            myConstants.getTestTitleString(),
+                            myConstants.getTestMessageString());
+
                 }
 
             }   // OnClick
         });
 
     }   // Main Method
+
+    // Get data from json service...
+    private class SynData extends AsyncTask<String, Void, String> {
+
+        // Explicit
+        private Context context; // Alt+Insert
+        private String titleString, messageString;
+
+        public SynData(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            // overide on post
+            try {
+                titleString = params[1];
+                messageString = params[2];
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(params[0]).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();    // *****
+
+            } catch (Exception e) {
+                Log.d("24octV1", "doInBackground --> " + e.toString());
+                return null;
+            }
+        }   // doInBackground
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("24octV1", "JSON --> " + s);
+        }
+    }   // SynData class
 }   // Main class
+
