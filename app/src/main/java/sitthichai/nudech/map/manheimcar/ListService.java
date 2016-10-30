5e41eb905e7324e573f9fe002426e8abdf141d99
SitthichaiN <sitthichai.n@gmail.com>
2016-10-30 09:58:41 +0700

    Get location

--- app/src/main/java/sitthichai/nudech/map/manheimcar/ListService.java
+++ app/src/main/java/sitthichai/nudech/map/manheimcar/ListService.java
@@ -1,6 +1,11 @@
 package sitthichai.nudech.map.manheimcar;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +18,9 @@ public class ListService extends AppCompatActivity {
     // Explicit
     private ListView listView;
     private String[] nameStrings, latStrings, lngStrings, imageStrings;
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private Double latADouble, lngADouble;
 
 
     @Override
@@ -42,15 +50,66 @@ public class ListService extends AppCompatActivity {
 
         // Create ListView
         OfficerAdapter officerAdapter = new OfficerAdapter(ListService.this,
-                nameStrings, latStrings, lngStrings, imageStrings);
+                                                            nameStrings,
+                                                            latStrings,
+                                                            lngStrings,
+                                                            imageStrings);
         listView.setAdapter(officerAdapter);
+
+        // Item click
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("24octV4", "onItemClick --> Click");
                 Intent intent = new Intent(ListService.this, DetailActivity.class);
                 startActivity(intent);
-            }   // OnItemClick
+            }
         });
 
+        // Setup for Location
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false); // Z-cordinate
+
+
     }   //main method
+
+    public Location myFindLocation(String strProvider) {
+        Location location = null;
+
+        if (locationManager.isProviderEnabled(strProvider)) {
+            locationManager.requestLocationUpdates(strProvider, 1000, 10, locationListener);
+            location = locationManager.getLastKnownLocation(strProvider);
+        } else {
+            Log.d("30octV1", "Error Cannot Find Location");
+        }
+        return location;
+    }
+
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            // ตรวจสอบ การเปลี่ยนตำแหน่ง
+            latADouble = location.getLatitude();
+            lngADouble = location.getLongitude();
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // enable network location..
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }; // locationListener
 }   // main
